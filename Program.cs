45b6f7d315d270706d7e7a8cc4ffada7f97fbfce
Ben <bensmiffy60@gmail.com>
2024-04-03 21:35:04 +0100

    Adding code to Github for CV

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("How much do you weigh in kg?");
+
+//Recieves input from user as a string
+string? strWeight = Console.ReadLine();
+
+double weight = 0;
+
+//Converts string to a double value
+if(strWeight != null){
+    weight = double.Parse(strWeight);
+} else {
+    Console.WriteLine("Please enter a Value");
+}
+
+//Used the console here to test if the double value was correct
+//Console.WriteLine($"{weight}");
+
+//Instantiating Class as object to access methods
+ProteinCalculator obj = new ProteinCalculator();
+
+//Calculating the amount of protein needed 
+double result = obj.CalculateDailyIntake(weight);
+
+//Printing value to User
+Console.WriteLine($"Your daily amount of protein is {result}.grams");
+
+class ProteinCalculator{
+    public double CalculateDailyIntake(double weight){
+        double proteinPerKg = 2.2;
+        return weight * proteinPerKg;
+    }
+
+}
